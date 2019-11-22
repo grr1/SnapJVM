e2c17b46c8dab48a32d9ea4edcb45633ea5f44f7
MuyuanL <56162314+MuyuanL@users.noreply.github.com>
2019-11-22 11:40:25 -0500

    Test case for if statements

---
+++ test/Compare.java
@@ -0,0 +1,26 @@
+
+class Compare {
+	public static void main(String args[]) {
+	    int i = 2;
+	    int j = 4;
+
+	    
+	    if(i == 0){
+		System.out.println("i == 0");
+	    }else if(i > 0){
+		System.out.println("i > 0");
+	    }else{
+		System.out.println("i < 0");
+	    }
+
+	    
+	    if(i == j){
+		System.out.println("i == j");
+	    }else if(i > j){
+		System.out.println("i > j");
+	    }else{
+		System.out.println("i < j");
+	    }
+	}
+}
+
