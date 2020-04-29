e148e101c4e0333d1bd22e9c9275c6a98ffd71f3
sri-cherukuri <srivikas99@gmail.com>
2020-04-29 09:55:19 -0400

    Better Helper.java

--- test_src/Helper.java
+++ test_src/Helper.java
@@ -2,11 +2,12 @@ class Helper {
     public static void main(String args[]) {
         int i = 5;
         i = helper(i);
-        System.out.println("Hello\n");
+        System.out.println("After helper\n");
         //System.out.println("i="+i);
     }
 
     public static int helper(int x) {
+        System.out.println("Inside helper\n");
         return x*3;
     }
     /*public static int helper() {
